3bab7c11175f164646a80ba3371e5fdd689cd4d2
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-02-08 14:56:07 +0000

    initial commit with simple socket set up

---
+++ src/server/Server.java
@@ -0,0 +1,31 @@
+package server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        // Set up server socket
+        ServerSocket serverSocket = new ServerSocket(15123);
+        // Wait for accepting client
+        Socket socket = serverSocket.accept();
+        System.out.println("Accepted connection: " + socket);
+        // Retrieve file to send
+        File transferFile = new File("helloworld.txt");
+        byte[] byteArray = new byte[(int) transferFile.length()];
+        FileInputStream fin = new FileInputStream(transferFile);
+        BufferedInputStream bin = new BufferedInputStream(fin);
+        bin.read(byteArray, 0, byteArray.length);
+        // Set up channel of communication with client
+        OutputStream os = socket.getOutputStream();
+        System.out.println("Sending Files...");
+        // Write data to output stream
+        os.write(byteArray, 0, byteArray.length);
+        os.flush();
+        // Close
+        socket.close();
+        System.out.println("File transfer complete.");
+    }
+}
